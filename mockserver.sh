9f898d32919819d25971a46b20dec7a6832db0bd
tardigrada777 <ntwrite@gmail.com>
2019-07-29 20:17:13 +0300

    Наброски по User model

---
+++ mockserver.sh
@@ -0,0 +1,2 @@
+clear
+json-server -w db.json
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Parcel is working');
+import { User } from './models/User';
+
+const user = new User({ id: 1 });
+user.fetch();
+
+console.log(user);
