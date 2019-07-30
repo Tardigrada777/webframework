e8ae45dd34840880fcfc180f7a9dfdb2f16b858d
tardigrada777 <ntwrite@gmail.com>
2019-07-30 17:29:05 +0300

    Реализован класс Attributes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { User } from './models/User';
+import { User, UserProps } from './models/User';
+import { Attributes } from './models/Attributes';
 
-const user = new User({ name: 'NEW NAME', age: 0 });
+// const user = new User({ name: 'NEW NAME', age: 0 });
+
+const user = new Attributes<UserProps>({
+  name: 'Alex',
+  age: 2
+});
 
 console.log(user);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   name?: string;
@@ -10,15 +11,11 @@ export interface UserProps {
 const rootUrl = 'http://localhost:3000/users';
 
 export class User {
+  public attributes: Attributes<UserProps>;
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) {}
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
