5755be61def97652e0e8cd45ad1d23107429f720
tardigrada777 <ntwrite@gmail.com>
2019-07-31 10:57:57 +0300

    Класс Collection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { User } from './models/User';
+import { Collection } from './models/Collection';
 
-const user = User.buildUser({ id: 1 });
-user.on('save', () => {
-  console.log(user);
+const collection = new Collection('http://localhost:3000/users');
+
+collection.on('change', () => {
+  console.log(collection);
 });
 
-user.fetch();
-console.log(user);
+collection.fetch();
